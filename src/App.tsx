4f0a4715933036542c54e97c865a1ead7df5ed31
Abbas Alwash <abbas@alwash.nl>
2020-08-21 17:08:31 +0200

    added bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -2,5 +2,12 @@ import './App.scss';
 import React from 'react';
 
 export default class App extends React.Component {
-    public render = (): JSX.Element => <h1>App</h1>;
+    public render = (): JSX.Element => {
+        return (
+            <React.Fragment>
+                <h1>App</h1>
+                <p className="h1">App</p>
+            </React.Fragment>
+        );
+    };
 }
