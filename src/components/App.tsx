26d1863e5e941521405804fe63e92cf173701f76
Abbas Alwash <abbas@alwash.nl>
2020-08-23 00:04:01 +0200

    add, edit and remove todo

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,67 @@
 import '../assets/scss/App.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ITodo from '../interfaces/ITodo';
 import React from 'react';
 import Todo from './Todo';
 import { faPlus} from '@fortawesome/free-solid-svg-icons';
 
-export default class App extends React.Component {
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface IAppProps {}
+
+interface IAppState {
+    todos: ITodo[];
+}
+
+export default class App extends React.Component<IAppProps, IAppState> {
+    private constructor(props: IAppProps) {
+        super(props);
+
+        this.state = {
+            todos: [
+                {
+                    id: Date.now(),
+                    value: 'My first todo'
+                }
+            ]
+        };
+    }
+
+    private addTodo = (): void => {
+        const todo: ITodo = {
+            id: Date.now(),
+            value: ''
+        };
+
+        this.setState({ todos: [...this.state.todos, todo] });
+    }
+
+    private removeTodo = (todoId: number): void => {
+        const todos = this.state.todos.filter(todo => todo.id !== todoId);
+
+        this.setState({ todos });
+    }
+
+    private editTod = (event: React.ChangeEvent<HTMLInputElement>, todoId: number): void => {
+        const value: string = event.target.value;
+        const todo: ITodo = {
+            id: todoId,
+            value 
+        };
+        const todos = this.state.todos.filter(todo => todo.id !== todoId);
+        todos.push(todo);
+
+        this.setState({ todos });
+    }
+
     public render = (): JSX.Element => {
         return (
             <>
-                <button className="btn btn-outline-primary" type="button"><FontAwesomeIcon icon={faPlus}/> add</button>
-                <Todo />
-                <Todo />
-                <Todo />
+                <button className="btn btn-outline-primary" type="button" onClick={this.addTodo}>
+                    <FontAwesomeIcon icon={faPlus}/> add
+                </button>
+                {this.state.todos.map(todo => {
+                    return <Todo todo={todo} key={todo.id} onChangeHandler={this.editTod} onClickHandler={this.removeTodo} />;
+                })}
             </>
         );
     };
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,13 +1,31 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ITodo from '../interfaces/ITodo';
 import React from 'react';
 import { faTrashAlt} from '@fortawesome/free-regular-svg-icons';
 
-export default class Todo extends React.Component {
+interface ITodoProps {
+    todo: ITodo;
+    onChangeHandler: (event: React.ChangeEvent<HTMLInputElement>, todoId: number) => void;
+    onClickHandler: (todoId: number) => void;
+}
+
+export default class Todo extends React.Component<ITodoProps> {
+    private constructor(props: ITodoProps) {
+        super(props);
+    }
+
     public render(): JSX.Element {
         return (
             <div className="input-group">
-                <input type="text" className="form-control" />
-                <button className="btn btn-outline-danger" type="button"><FontAwesomeIcon icon={faTrashAlt}/> delete</button>
+                <input
+                    type="text"
+                    className="form-control"
+                    defaultValue={this.props.todo.value}
+                    onChange={(event): void => this.props.onChangeHandler(event, this.props.todo.id)}
+                />
+                <button className="btn btn-outline-danger" type="button" onClick={(): void => this.props.onClickHandler(this.props.todo.id)}>
+                    <FontAwesomeIcon icon={faTrashAlt}/> delete
+                </button>
             </div>
         );
     }
