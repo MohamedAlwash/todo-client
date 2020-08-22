a568aa6c466e7e07065573ba12feaa1cc4a09ed1
Mohamed <mohamedi.c.t1996@gmail.com>
2020-08-22 17:15:20 +0200

    Font Awesome works now

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import '../assets/scss/App.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import Todo from './Todo';
+import { faPlus} from '@fortawesome/free-solid-svg-icons';
 
 export default class App extends React.Component {
     public render = (): JSX.Element => {
         return (
             <>
-                <button className="btn btn-outline-primary" type="button">add</button>
+                <button className="btn btn-outline-primary" type="button"><FontAwesomeIcon icon={faPlus}/> add</button>
                 <Todo />
                 <Todo />
                 <Todo />
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,11 +1,13 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { faTrashAlt} from '@fortawesome/free-regular-svg-icons';
 
 export default class Todo extends React.Component {
     public render(): JSX.Element {
         return (
             <div className="input-group">
                 <input type="text" className="form-control" />
-                <button className="btn btn-outline-danger" type="button">delete</button>
+                <button className="btn btn-outline-danger" type="button"><FontAwesomeIcon icon={faTrashAlt}/> delete</button>
             </div>
         );
     }
